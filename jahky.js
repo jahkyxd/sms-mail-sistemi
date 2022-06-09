b26288eecd982928af2809315fa7c00fa38aeb15
jahkyxd <82198115+jahkyxd@users.noreply.github.com>
2022-06-09 16:58:52 +0300

    Update jahky.js

--- jahky.js
+++ jahky.js
@@ -3,7 +3,7 @@ import Loaders from "./src/Base/Load.js";
 import Jahky from "./src/Base/Jahky.Client.js";
 global.client = new Jahky();
 
-// system requred \\
+// system required \\
 
 Login.On();
 Loaders.LoadEvents("./src/events");
